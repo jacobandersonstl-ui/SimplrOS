89e966b38c7fd9f6ec57cc97bb0ccc70131507d7
Jacob Anderson <jacobandersonstl@gmail.com>
2026-05-10 00:52:25 -0500

    Kernel: Front Renderer Working, Drawing text to Framebuffer

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "font.h"
 
 typedef struct {
     uint64_t framebuffer;
@@ -7,20 +8,30 @@ typedef struct {
     uint32_t pixels_per_scanline;
 } BootInfo;
 
+void draw_char(volatile uint32_t *fb, uint32_t pixels_per_scanline,
+               int x, int y, char c, uint32_t color)
+{
+    const uint8_t *glyph = font[(uint8_t)c];
+    for (int row = 0; row < 8; row++) {
+        for (int col = 0; col < 8; col++) {
+            if (glyph[row] & (0x80 >> col)) {
+                fb[(y + row) * pixels_per_scanline + (x + col)] = color;
+            }
+        }
+    }
+}
+
 void kernel_main(BootInfo *info)
 {
     volatile uint32_t *fb = (volatile uint32_t *)info->framebuffer;
 
-    // Fill screen black
     for (uint32_t i = 0; i < info->height * info->pixels_per_scanline; i++) {
         fb[i] = 0x000000;
     }
 
-    // Draw a white rectangle in the top left
-    for (uint32_t y = 10; y < 50; y++) {
-        for (uint32_t x = 10; x < 300; x++) {
-            fb[y * info->pixels_per_scanline + x] = 0xFFFFFF;
-        }
+    const char *msg = "SimplrOS";
+    for (int i = 0; msg[i]; i++) {
+        draw_char(fb, info->pixels_per_scanline, i * 9, 10, msg[i], 0xFFFFFF);
     }
 
     while(1) {}
